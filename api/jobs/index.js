362a797538e99d1f578dd0bd765bd23c0c3d7c02
Isabella Serna <isabella.serna.ramirez@gmail.com>
2021-03-17 18:34:21 -0500

    Add new features
    -getLocations => obtain all locations of job offers deduplicated
    -searchByKeywod => obtain all jobs that contains keywords in the title of job offers
    -searchByType => obtain all jobs filtered by type
    -searchByTechnology => obtain all jobs that have a certain technology

--- api/jobs/index.js
+++ api/jobs/index.js
@@ -3,5 +3,9 @@ const controller = require("./jobController");
 const router = new Router();
 
 router.get("/", controller.getList);
+router.get("/locations", controller.getLocations);
+router.get("/technologies/:tecnology", controller.searchByTechnology);
+router.get("/keywods/:keyword", controller.searchByKeyword);
+router.get("/types/:type", controller.searchByType);
 
 module.exports = router;
--- api/jobs/jobController.js
+++ api/jobs/jobController.js
@@ -1,9 +1,67 @@
 const jobs = require("./jobs.json");
 
 const getList = (req, res) => {
-  res.json(jobs);
+  if (jobs instanceof Array) {
+    return res.status(200).json(jobs);
+  }
+  res.status(404).json([]);
+};
+
+const getLocations = (req, res) => {
+  const locations = [];
+  if (jobs instanceof Array) {
+    jobs.forEach((job) => {
+      if (!locations.includes(job.location)) {
+        locations.push(job.location);
+      }
+    });
+  }
+  res.status(200).json(locations);
+};
+
+const searchByKeyword = (req, res) => {
+  const keywords = req.params ? req.params.keyword : "";
+  const jobFilter = [];
+  const partsKeyword = keywords.split(" ");
+
+  if (jobs instanceof Array) {
+    jobs.forEach((job) => {
+      const containsKeywords = partsKeyword.every((keyword) => {
+        return job.title.includes(keyword);
+      });
+
+      if (containsKeywords) {
+        jobFilter.push(job);
+      }
+    });
+  }
+  res.json(jobFilter);
+};
+
+const searchByType = (req, res) => {
+  const type = req.params ? req.params.type : "";
+  let jobFilter = [];
+  if (jobs instanceof Array) {
+    jobFilter = jobs.filter((job) => job.type.toUpperCase() === type);
+  }
+  res.status(200).json(jobFilter);
+};
+
+const searchByTechnology = (req, res) => {
+  const technologySearched = req.params ? req.params.tecnology : "";
+  let jobFilter = [];
+  if (jobs instanceof Array) {
+    jobFilter = jobs.filter((job) =>
+      job.stack ? job.stack.includes(technologySearched) : false
+    );
+  }
+  res.status(200).json(jobFilter);
 };
 
 module.exports = {
   getList,
+  getLocations,
+  searchByType,
+  searchByTechnology,
+  searchByKeyword,
 };
